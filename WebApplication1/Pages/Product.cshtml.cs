f2976390ba230e3c153d528aedff381bc35cd4fd
N0844425 <76251581+mxxch@users.noreply.github.com>
2021-04-30 18:20:16 +0300

    submission1

--- WebApplication1/Pages/Product.cshtml.cs
+++ WebApplication1/Pages/Product.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApplication1.Models;
@@ -10,21 +11,66 @@ namespace WebApplication1.Pages
 {
     public class ProductModel : PageModel
     {
+
+
+
         [BindProperty(SupportsGet = true)]
         public int Id { get; set; }
 
         [BindProperty]
         public Product Products { get; set; }
 
+        private readonly UserManager<AppUser> _userManager;
+
         public readonly AppDataContext _db;
 
-        public ProductModel(AppDataContext db)
+        public ProductModel(AppDataContext db, UserManager<AppUser> userManager)
         {
+            _userManager = userManager;
             _db = db;
         }
+
+        public Product product { get; set; }
+
+        public Cart cart { get; set; }
+        public List<Cart> carts { get; set; }
         public void OnGet()
         {
             Products = _db.Product.Find(Id);
+        }
+
+        public IActionResult OnGetCart(int Id)
+        {
+            product = new Product();
+            cart = new Cart();
+            carts = _db.Cart.Where(e => e.UserID == _userManager.GetUserId(User)).ToList();
+            product = _db.Product.Find(Id);
+
+
+            foreach (var item in carts)
+            {
+                if (item.Name == product.Name && item.UserID == _userManager.GetUserId(User))
+                {
+                    item.Quantity++;
+                    _db.SaveChanges();
+                    return RedirectToPage("shop");
+
+                }
+
+            }
+
+
+            cart.Name = product.Name;
+            cart.price = product.Price;
+            cart.Quantity = 1;
+            cart.ProductImage = product.ProductImage;
+            cart.UserID = _userManager.GetUserId(User);
+
+            _db.Cart.Add(cart);
+            _db.SaveChanges();
+            return RedirectToPage("shop");
+
+
         }
     }
 }
